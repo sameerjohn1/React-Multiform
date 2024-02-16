a5ff2cd2c73a905a6f6a9387fc8d98aef8c7f9b2
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-02-16 10:13:16 +0500

    setting states

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [page, setPage] = useState(0);
+
+  const FormTitles = ["Sign Up", "Personal Info", "Other"];
   return (
     <div className="form">
       <div className="progressbar"></div>
       <div className="form-container">
-        <div className="header"></div>
+        <div className="header">
+          <h1>{FormTitles[page]}</h1>
+        </div>
         <div className="body"></div>
         <div className="footer">
-          <button>Prev</button>
-          <button>Next</button>
+          <button
+            disabled={page == 0}
+            onClick={() => {
+              setPage((currPage) => currPage - 1);
+            }}
+          >
+            Prev
+          </button>
+          <button
+            disabled={page == FormTitles.length - 1}
+            onClick={() => {
+              setPage((currPage) => currPage + 1);
+            }}
+          >
+            Next
+          </button>
         </div>
       </div>
     </div>
