03326992fbf119773ff983be7bcea6cd0bc73f59
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-02-26 12:25:07 +0500

    handling user state

--- src/Components/OtherInfo.jsx
+++ src/Components/OtherInfo.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
-const OtherInfo = () => {
+const OtherInfo = ({ formData, setFormData }) => {
   return (
     <div className="other-info-container">
-      <input type="text" placeholder="Nathionality..." />
-      <input type="text" placeholder="Other..." />
+      <input
+        type="text"
+        placeholder="Nathionality..."
+        name="nationality"
+        value={formData.nationality}
+        onChange={(event) =>
+          setFormData({ ...formData, nationality: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Other..."
+        name="other"
+        value={formData.other}
+        onChange={(event) =>
+          setFormData({ ...formData, other: event.target.value })
+        }
+      />
     </div>
   );
 };
