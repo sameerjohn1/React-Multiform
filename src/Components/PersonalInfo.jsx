56d8de8358df509d36da5bca98b82c3ea116236b
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-02-26 12:25:25 +0500

    handling user states

--- src/Components/PersonalInfo.jsx
+++ src/Components/PersonalInfo.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 
-const PersonalInfo = () => {
+const PersonalInfo = ({ formData, setFormData }) => {
   return (
     <div className="personal-info-container">
-      <input type="text" placeholder="FirstName..." />
-      <input type="text" placeholder="LastName..." />
-      <input type="text" placeholder="Username..." />
+      <input
+        type="text"
+        placeholder="FirstName..."
+        name="firstName"
+        value={formData.firstName}
+        onChange={(event) =>
+          setFormData({ ...formData, firstName: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="LastName..."
+        name="lastName"
+        value={formData.lastName}
+        onChange={(event) =>
+          setFormData({ ...formData, lastName: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Username..."
+        name="username"
+        value={formData.username}
+        onChange={(event) =>
+          setFormData({ ...formData, username: event.target.value })
+        }
+      />
     </div>
   );
 };
