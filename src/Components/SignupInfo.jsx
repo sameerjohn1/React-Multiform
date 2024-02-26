dbd77b0f9457190ec1c9df2f260da70cba6608b1
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-02-26 12:25:35 +0500

    handling user states

--- src/Components/SignupInfo.jsx
+++ src/Components/SignupInfo.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 
-const SignupInfo = () => {
+const SignupInfo = ({ formData, setFormData }) => {
   return (
     <div className="sign-up-container">
-      <input type="text" placeholder="Email..." />
-      <input type="text" placeholder="Password..." />
-      <input type="text" placeholder="Confirm Password..." />
+      <input
+        type="text"
+        placeholder="Email..."
+        name="email"
+        value={formData.email}
+        onChange={(event) =>
+          setFormData({ ...formData, email: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Password..."
+        name="password"
+        value={formData.password}
+        onChange={(event) =>
+          setFormData({ ...formData, password: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Confirm Password..."
+        name="confirmPassword"
+        value={formData.confirmPassword}
+        onChange={(event) =>
+          setFormData({ ...formData, confirmPassword: event.target.value })
+        }
+      />
     </div>
   );
 };
